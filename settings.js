ebf7d0d660e53d66551892396bef9cd3f0133bf9
DrTruth0456 <145074775+DrTruth0456@users.noreply.github.com>
2025-03-18 11:44:36 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Kanyambo', // Your name
-  ownerNumber: '255684067706', //Your number
+  botName: "Karter Bot",
+  botOwner: 'KxOfficial', // Your name
+  ownerNumber: '255743929934', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
